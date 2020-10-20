849dedefece41e36799135569c8c4fa0862aa277
isdabelle <isabelle.pumford@epfl.ch>
2020-10-20 09:40:59 +0200

    Platoon Finding updated with Gotthard and Denges added

--- structConversion.m
+++ structConversion.m
@@ -1,18 +1,14 @@
 % Modifying the tables so that they are saved as structs
+% Also running the processed data matrices through the classified to obtain
+% the truck classification of each vehicle
 
-%first load the tables
-% Ceneri408 = importdata('Ceneri408_2017.mat');
-% Ceneri409 = importdata('Ceneri409_2017.mat');
-% 
-% Ceneri408_struct = struct('table', struct(Ceneri408), 'columns', {struct(Ceneri408).varDim.labels});
-% save('Ceneri408_struct.mat', 'Ceneri408_struct')
-% 
-% Ceneri409_struct = struct('table', struct(Ceneri409), 'columns', {struct(Ceneri409).varDim.labels});
-% save('Ceneri409_struct.mat', 'Ceneri409_struct')
 
 %For each year in Ceneri import the matrix and make a struct from it
+
 for year = 2011:2017
-    data = importdata(sprintf('Ceneri_%d.mat', year));
-    Ceneri_struct = struct('table', struct(data), 'columns', {struct(data).varDim.labels});
-    save(sprintf('Ceneri_%d_struct.mat', year), 'Ceneri_struct')
+    data = importdata(sprintf('Gotthard_%d.mat', year));
+    %Classification of the processed data
+    data_classified = Classify(data);
+    Gotthard_struct = struct('table', struct(data_classified), 'columns', {struct(data_classified).varDim.labels});
+    save(sprintf('Gotthard_%d_struct.mat', year), 'Gotthard_struct')
 end 
