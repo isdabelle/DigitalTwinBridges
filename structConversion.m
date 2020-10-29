7934bc0bdd589dafc48b3d729ff557fb51f8f067
isdabelle <isabelle.pumford@epfl.ch>
2020-10-29 14:06:33 +0100

    Adding count seasonality, WIMConversion and platoon finding class breakdown

--- structConversion.m
+++ structConversion.m
@@ -5,10 +5,14 @@
 
 %For each year in Ceneri import the matrix and make a struct from it
 
-for year = 2011:2017
-    data = importdata(sprintf('Gotthard_%d.mat', year));
-    %Classification of the processed data
+station = "Gotthard";
+
+start_y = 2018; end_y = 2019;
+
+for year = start_y:end_y
+    data = importdata(sprintf('%s_%d.mat', station, year));
+    %Classification of the processed dataS
     data_classified = Classify(data);
     Gotthard_struct = struct('table', struct(data_classified), 'columns', {struct(data_classified).varDim.labels});
-    save(sprintf('Gotthard_%d_struct.mat', year), 'Gotthard_struct')
+    save(sprintf('%s_%d_struct.mat', station, year), sprintf('%s_struct', station))
 end 
